198d648865ae2d44755f6484d86aa31a6e8a1b2c
christoli <mywptask@gmail.com>
2023-08-10 13:50:38 +0000

    HTML and CSS code completed

---
+++ app.js
@@ -0,0 +1,20 @@
+// regular expression for validation
+const strRegex =  /^[a-zA-Z\s]*$/; // containing only letters
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+/* supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
+const digitRegex = /^\d+$/;
+
+// -------------------------------------------------- //
+
+const countryList = document.getElementById('country-list');
+const fullscreenDiv = document.getElementById('fullscreen-div');
+const modal = document.getElementById('modal');
+const addBtn = document.getElementById('add-btn');
+const closeBtn = document.getElementById('close-btn');
+const modalBtns = document.getElementById('modal-btns');
+const form = document.getElementById('modal');
+const addrBookList = document.querySelector('#addr-book-list tbody');
+
+// -------------------------------------------------- //
+let addrName = firstName = lastName = email = phone = streetAddr = postCode = city = country = labels = "";
